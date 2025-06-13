65f5d81f6d75a44b1a96f654e7eb495b8130c964
Yang-YJY <1072937203@qq.com>
2025-06-13 17:11:17 +0800

    added an off-heap hash bucket structure for StateMap and made some directory adjustment

---
+++ src/main/java/org/apache/flink/runtime/state/heap/OffHeapBucketTable.java
@@ -0,0 +1,4 @@
+package org.apache.flink.runtime.state.heap;
+
+public class OffHeapBucketTable {
+}
--- src/main/java/org/apache/flink/runtime/state/heap/utils/UnsafeUtils.java
+++ src/main/java/org/apache/flink/runtime/state/heap/utils/UnsafeUtils.java
@@ -1,4 +1,4 @@
-package org.apache.flink.runtime.heap.space;
+package org.apache.flink.runtime.state.heap.space;
 
 import sun.misc.Unsafe;
 
